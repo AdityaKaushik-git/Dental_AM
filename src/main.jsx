4cff0a5fb3ac1a123cff49b9e3fa275ff9e52468
Aditya Kaushik <adityakaushik1200@gmail.com>
2026-07-18 22:11:47 +0530

    Setup GitHub Pages deployment

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import './styles/index.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <HelmetProvider>
-      <BrowserRouter>
+      <BrowserRouter basename="/Dental_AM">
         <App />
       </BrowserRouter>
     </HelmetProvider>
